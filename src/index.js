4d8e5d396d07d4c7eb6cca5ace38bd9c86dd528b
Yan Susanto <yansusanto@gmail.com>
2022-05-18 17:55:49 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {Web3ReactProvider} from "@web3-react/core";
+import Web3 from "web3";
+
+function getLibrary(provider) {
+	return new Web3(provider);
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Web3ReactProvider getLibrary={getLibrary}>
+			<App />
+		</Web3ReactProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+reportWebVitals();
