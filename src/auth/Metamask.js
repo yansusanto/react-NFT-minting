1049fec2ed435f1ef1538d1ce7f1e248befe784a
Yan Susanto <yansusanto@gmail.com>
2022-05-21 17:30:50 +0800

    ADD: eth balance

--- src/auth/Metamask.js
+++ src/auth/Metamask.js
@@ -1,7 +1,7 @@
 import {useState, useEffect, useRef} from "react";
 import {useWeb3React} from "@web3-react/core";
 import {injected} from "../wallet/Connector";
-import web3 from "web3";
+import Web3 from "web3";
 import items from "../api/items";
 import Swal from "sweetalert2";
 import {Modal} from "bootstrap";
@@ -15,6 +15,7 @@ export default function Auth() {
 	const {active, account, library, activate, deactivate} = useWeb3React();
 	const [modal, setModal] = useState(null);
 	const [data, setData] = useState(false);
+	const [balance, setBalance] = useState(null);
 	const nftModal = useRef();
 	const avatar = useRef();
 
@@ -36,12 +37,28 @@ export default function Auth() {
 		setModal(new Modal(nftModal.current));
 	}, []);
 
+	// ----------- if on mobile device --------
 	function isMobile() {
 		return "ontouchstart" in window || "onmsgesturechange" in window;
 	}
 
 	const dappUrl = "boredmonkey.netlify.app";
 	const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
+
+	// ----------- detect current provider --------
+	const detectCurrentProvider = () => {
+		let provider;
+		if (window.ethereum) {
+			provider = window.ethereum;
+		} else if (window.web3) {
+			provider = window.web3.currentProvider;
+		} else {
+			console.log(
+				"Non-Ethereum browser detected. You should consider trying MetaMask!"
+			);
+		}
+		return provider;
+	};
 
 	async function connect() {
 		try {
@@ -66,6 +83,18 @@ export default function Auth() {
 					position: "top-end",
 					timer: 1500,
 				});
+			}
+			const currentProvider = detectCurrentProvider();
+			if (currentProvider) {
+				await currentProvider.request({method: "eth_requestAccounts"});
+				const web3 = new Web3(currentProvider);
+				const userAccount = await web3.eth.getAccounts();
+				const account = userAccount[0];
+				// ----------- get wallet balance --------
+				let ethBalance = await web3.eth.getBalance(account);
+				// ----------- conversion to Wei units --------
+				ethBalance = web3.utils.fromWei(ethBalance, "ether");
+				setBalance(ethBalance);
 			}
 		} catch (ex) {
 			console.log(ex);
@@ -95,7 +124,7 @@ export default function Auth() {
 			to: myAccount,
 			from: account,
 			// ----------- conversion to Wei units --------
-			value: web3.utils.toWei(data.price, "ether"),
+			value: Web3.utils.toWei(data.price, "ether"),
 			gas: 85000,
 			gasLimit: "100000",
 		};
@@ -125,24 +154,35 @@ export default function Auth() {
 						className="navbar-brand d-flex align-items-center"
 					>
 						{!active ? (
-							<img
-								src={logo}
-								alt="Bored Monkey"
-								width="30"
-								height="30"
-								className="me-2"
-							/>
+							<>
+								<img
+									src={logo}
+									alt="Bored Monkey"
+									width="30"
+									height="30"
+									className="me-2"
+								/>
+								Bored Monkey
+							</>
 						) : (
-							<div ref={avatar} className="me-2 d-flex"></div>
+							<>
+								<div ref={avatar} className="me-2 d-flex"></div>
+								<span className=" d-none d-md-block">
+									{account &&
+										account.substring(0, 6) +
+											"..." +
+											account.substring(
+												account.length - 6
+											)}
+								</span>
+							</>
 						)}
-						Bored Monkey
 					</a>
 					<form className="d-flex align-items-center">
-						<span className="small text-muted me-3 d-none d-md-block">
-							{account &&
-								account.substring(0, 6) +
-									"..." +
-									account.substring(account.length - 6)}
+						<span className="text-success fw-bold me-3">
+							{active &&
+								balance &&
+								balance.substring(0, 8) + " ETH"}
 						</span>
 						{active ? (
 							<button
